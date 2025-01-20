4513cab758e2941b3dab1c635a601c699b814c64
hysngg <hs.kim@teamsparta.co>
2025-01-20 13:11:10 +0900

    feat : react-router-dom 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,23 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
-import { SeatingChartPage } from "../pages/SeatingChartPage";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Route,
+} from "react-router-dom";
+import { DefaultLayout } from "../components/Layout/DefaultLayout";
 import { AdminPage } from "../pages/AdminPage";
+import { SeatingChartPage } from "../pages/SeatingChartPage";
 
-export const router = createBrowserRouter([
-  {
-    path: "/seating-chart",
-    element: <SeatingChartPage />,
-  },
-  {
-    path: "/",
-    element: (
-      <div className="flex flex-col items-center justify-center h-dvh gap-2">
-        <a href="/seating-chart">좌석 배치도로 가기</a>
-        <a href="/admin">관리자 페이지로 가기</a>
-      </div>
-    ),
-  },
-  {
-    path: "/admin",
-    element: <AdminPage />,
-  },
-]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<DefaultLayout />}>
+        <Route path="/seating-chart" element={<SeatingChartPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Route>
+      <Route
+        path="/"
+        element={
+          <div className="flex flex-col items-center justify-center h-dvh gap-2">
+            <Link to="/seating-chart">좌석 배치도로 가기</Link>
+            <Link to="/admin">관리자 페이지로 가기</Link>
+          </div>
+        }
+      />
+    </>
+  )
+);
