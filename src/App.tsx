b50d069d47d550b3c8de4d3efced37946a7638d8
hysngg <hs.kim@teamsparta.co>
2025-03-13 21:21:28 +0900

    feat : queryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { RouterProvider } from "react-router-dom";
 import { Container } from "./components/Layout/Container";
 import { router } from "./routes/router";
 import { StackProvider } from "@teamsparta/stack-core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <Container>
       <StackProvider theme="sccLight">
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </StackProvider>
     </Container>
   );
